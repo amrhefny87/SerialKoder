55df6ed5620ca9a031ba0ef8a532b0a7e9b9fa16
Andres Patiño <andresp199519@gmail.com>
2021-04-20 20:39:40 +0200

    Función para eliminar concursante

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
-import {listaNombres} from "./data.js"
+import {listaNombres, purgatorio} from "./data.js"
 
 let boton = document.getElementById("aleatorio")
 
 boton.addEventListener("click", () =>{
     let aleatorio = listaNombres[Math.floor(Math.random() * listaNombres.length)]
+
+    purgatorio.push(aleatorio)
     console.log(aleatorio)
-    listaNombres.splice(aleatorio, -1)
+    console.log(purgatorio)
+    for (let index = 0; index < listaNombres.length; index++) {
+        if (listaNombres[index] === aleatorio) {
+            listaNombres.splice(index, 1)  
+        }
+    }
     console.log(listaNombres)
+    if(listaNombres.length == 0){
+        alert("Felicitaciones, te los has cargado a todos")
+    }
+    
 })
 
