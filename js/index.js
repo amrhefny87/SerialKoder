0209b4fdf652a6984067fd08d0d2f16d7a509841
Granaino BUAB <josemiguel@quesada.eu>
2021-04-21 12:44:07 +0200

    cambio filtro

--- js/index.js
+++ js/index.js
@@ -5,7 +5,13 @@ let boton = document.getElementById("aleatorio")
 boton.addEventListener("click", () =>{
     let aleatorio = listaNombres[Math.floor(Math.random() * listaNombres.length)]
     console.log(aleatorio)
-    listaNombres.splice(aleatorio, -1)
+
+    for (let index = 0; index < listaNombres.length; index++) {
+        if (listaNombres[index] === aleatorio) {
+            listaNombres.splice(index, 1)  
+        }
+        
+    }
     console.log(listaNombres)
 })
 
