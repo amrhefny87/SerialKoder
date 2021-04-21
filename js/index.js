de064300c9f979b757dad86fa1b72548e9012545
Andres Patiño <andresp199519@gmail.com>
2021-04-21 12:25:58 +0200

    Intento de refactorización

--- js/index.js
+++ js/index.js
@@ -3,26 +3,41 @@ import {listaNombres, purgatorio} from "./data.js"
 let boton = document.getElementById("aleatorio")
 
 boton.addEventListener("click", () =>{
-    let aleatorio = listaNombres[Math.floor(Math.random() * listaNombres.length)]
+gameOver()
+killCode()
 
-    purgatorio.push(aleatorio)
-    console.log(aleatorio)
+   
     console.log(purgatorio)
+    console.log(listaNombres)
+})
+
+
+
+function killCode(){
+    let aleatorio = listaNombres[Math.floor(Math.random() * listaNombres.length)]
     for (let index = 0; index < listaNombres.length; index++) {
         if (listaNombres[index] === aleatorio) {
             listaNombres.splice(index, 1)  
         }
     }
-    console.log(listaNombres)
+    console.log(aleatorio)
+}
+
+function addPurgatorio(aleatorio){
+    purgatorio.push(aleatorio)
+   console.log(aleatorio)
+}
+
+
+let reinicio = document.getElementById("reiniciar")
+
+function gameOver(){
     if(listaNombres.length == 0){
         alert("Felicitaciones, te los has cargado a todos")
         location.reload()
     }
-    
-})
+}
 
-let reinicio = document.getElementById("reiniciar")
-
-reinicio.addEventListener("click", () =>{
+    reinicio.addEventListener("click", () =>{
     location.reload()
 })
